3c0a5792662b3bfca3866551475277f4acab031e
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2014-07-06 09:03:21 +0530

    jsonp implementation added

--- loader.js
+++ loader.js
@@ -1,98 +1,73 @@
-(function(root){
-    function Promise() {
-        var callbacks = [],
-            promise = {
-                resolve: resolve,
-                reject: reject,
-                then: then,
-                safe: {
-                    then: function safeThen(resolve, reject) {
-                        promise.then(resolve, reject);
-                    }
-                }
-            };
-
-        function complete(type, result) {
-            promise.then = type === 'reject'
-                ? function (resolve, reject) { reject(result); }
-                : function (resolve) { resolve(result); };
-
-            promise.resolve = promise.reject = function () { /*do nothing*/ };
-
-            var i = 0, cb;
-            while (cb = callbacks[i++]) { cb[type] && cb[type](result); }
-
-            callbacks = null;
-        }
-
-        function resolve(result) {
-            complete('resolve', result);
-        }
-
-        function reject(err) {
-            complete('reject', err);
-        }
-
-        function then(resolve, reject) {
-            callbacks.push({ resolve: resolve, reject: reject });
-        }
-
-        return promise;
-    }
-
-    var promiseIndex = {};
-    var getPromise = function(url){
-        var promise = promiseIndex[url];
-        if(!promise){
-            promise = new Promise();
-        }
-        return promise;
-    }
-
-    var getData = function(url, callback){
+(function (root) {
+    var JSONPCounter = 0;
+    var getData = function (url, callback) {
         var xmlHTTP = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("MicrosoftXMLHTTP");
-        var promise =  getPromise(url);
-        xmlHTTP.onreadystatechange = function(){
-            if(this.readyState == 4 && this.status == 200){
-                promise.resolve(this.responseText);
-                if(callback){callback(this.responseText);}
+        xmlHTTP.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                if (callback) {callback(this.responseText);}
             }
-
         }
         xmlHTTP.open("GET", url, true);
         xmlHTTP.send();
-        return promise;
     }
 
-    var count = 10, currentIndex = 0, loadedIndex = 0, threshold = 5, onLoaded = function(){},
-        getUrl = function(){
-            return 'data/data-'+loadedIndex+'-'+(loadedIndex+count)+'.json'
+    var loadJSONP = function(url){
+        var script = document.createElement('script'),
+            done = false;
+        script.src = url;
+        script.async = true;
+
+        script.onload = script.onreadystatechange = function() {
+            if ( !done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") ) {
+                done = true;
+                script.onload = script.onreadystatechange = null;
+                if ( script && script.parentNode ) {
+                    script.parentNode.removeChild( script );
+                }
+            }
+        };
+        
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild( script );
+    }
+    var getDataJSONP = function(url, callback){
+        var uniqueName =   "callback_json" + (++JSONPCounter);
+        window[ uniqueName ] = function(data){
+            callback(data);
+        }
+        loadJSONP(url+'&callback='+uniqueName);
+    }
+
+    var count = 10, currentIndex = 0, loadedIndex = 0, threshold = 5, onLoaded = function () {},
+        getUrl = function () {
+            return 'data/data-' + loadedIndex + '-' + (loadedIndex + count) + '.json'
         },
-        loadNextData = function(){
-            var promise = getData(getUrl());
-            promise.then(function(resp){
+        loadNextData = function () {
+            getDataJSONP(getUrl(), function(resp){
                 loadedIndex += count;
                 onLoaded(resp)
-            })
+            });
         },
-        checkBackgroundLoad = function(){
-            if(loadedIndex - currentIndex < threshold){
+        checkBackgroundLoad = function () {
+            if (loadedIndex - currentIndex < threshold) {
                 loadNextData()
             }
         },
-        bumpUpIndex = function(){currentIndex++; checkBackgroundLoad()},
-        bumpDownIndex = function(){if(currentIndex>0)currentIndex--;}
-
+        bumpUpIndex = function () {
+            currentIndex++;
+            checkBackgroundLoad()
+        },
+        bumpDownIndex = function () {if (currentIndex > 0)currentIndex--;}
 
 
     root.loader = {
-        init:function(mediator){
+        init: function (mediator) {
             onLoaded = mediator.onLoaded;
             mediator.bumpUpIndex = bumpUpIndex;
             mediator.bumpDownIndex = bumpDownIndex;
             checkBackgroundLoad();
         },
-        logIndex: function(){
+        logIndex: function () {
             console.log(currentIndex, loadedIndex, threshold);
         }
     }
